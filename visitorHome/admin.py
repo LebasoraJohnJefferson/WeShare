f286192577b3d200b9469be2fe120ff1b5c1e57e
unknown <johnjefferson.lebasora@evsu.edu.ph>
2022-04-16 12:15:14 +0800

    feat(db): add table users in db

--- visitorHome/admin.py
+++ visitorHome/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import ExtendUser
 
 # Register your models here.
+
+@admin.register(ExtendUser)
+class ExtendUserAdmin(admin.ModelAdmin):
+    list_display= ['user','first_name','last_name','gender','email','profile','slug','is_active','created','updated'] 
+    list_filter = ['user']
+    prepopulated_fields = {'slug':('user',)}   
--- visitorHome/models.py
+++ visitorHome/models.py
@@ -1,3 +1,23 @@
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ExtendUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name= models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    profile = models.ImageField(upload_to="images/profiles")
+    slug = models.SlugField(max_length=255)
+    is_active=models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural="Users_Fields"
+
+    def __str__(self):
+        return self.user
