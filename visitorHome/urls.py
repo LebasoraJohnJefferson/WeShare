5d83557183099f581db297787d4e6d58fcd1070a
unknown <johnjefferson.lebasora@evsu.edu.ph>
2022-04-17 02:05:17 +0800

    feat(logout):adding functionality to logout links

--- visitorHome/urls.py
+++ visitorHome/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.visitor_home , name='visitor-home'),
     path('login', views.loginPage , name="Login"),
+    path('logout', views.logoutPage , name="Logout"),
     path('signup', views.signup , name="Signup"),
     path('profile', views.profile , name="Profile"),
 ]
--- visitorHome/views.py
+++ visitorHome/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from .forms import UserForm
 # Create your views here.
 
 
 def visitor_home(request):
-    return render(request , 'visitorHome/visitor-home.html' , { 'lebs':'300' })
+    return render(request , 'visitorHome/visitor-home.html' , { 
+        'lebs':'300',
+        'text': 'login',
+        'url':'Login',
+        'is_show_option':True
+     })
 
 def loginPage(request):
     if request.method == 'POST':
@@ -26,11 +32,20 @@ def loginPage(request):
             messages.error(request,"Account does not Exist")
             return redirect('Login')
 
-    context={}
-    return render(request , 'visitorHome/auth/login.html',context)
+    return render(request , 'visitorHome/auth/login.html',{ 
+        'url': 'visitor-home',
+        'text':'home','is_show_option':True
+    })
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('visitor-home')
 
 def signup(request):
     return render(request , 'visitorHome/auth/signup.html')
 
 def profile(request):
-    return render(request, 'visitorHome/profile.html')
+    return render(request, 'visitorHome/profile.html',{
+        'is_show_option':False
+    })
